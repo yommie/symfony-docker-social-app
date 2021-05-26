fcf82052b516396dcd80dc2a3e8e1ec0f633b8f8
Yommie <yommie76@gmail.com>
2021-05-26 20:56:25 +0100

    Added admin publish and unpublish posts endpoints

--- app/src/Controller/Api/V1/Admin/PostController.php
+++ app/src/Controller/Api/V1/Admin/PostController.php
@@ -5,10 +5,14 @@ namespace App\Controller\Api\V1\Admin;
 use App\Enum\Date;
 use App\Http\ApiResponse;
 use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\Post\Exception\PostNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class PostController extends AbstractController
 {
@@ -35,6 +39,96 @@ class PostController extends AbstractController
             [
                 "posts" => $posts
             ]
+        );
+    }
+
+
+
+
+
+    /**
+     * @Route("/admin/post/publish", methods={"PATCH"})
+     */
+    public function publish(
+        Request $request,
+        PostRepository $postRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $postId = $request->get("post_id");
+
+        if ($postId === null) {
+            throw new BadRequestHttpException("'post_id' must be present in request body");
+        }
+
+        try {
+            $post = $postRepository->find($postId);
+
+            if ($post === null) {
+                throw new PostNotFoundException();
+            }
+
+            $this->denyAccessUnlessGranted("PUBLISH_POST", $post);
+
+            $post->setIsPublished(true);
+
+            $entityManager->flush();
+        } catch (PostNotFoundException) {
+            return new ApiResponse(
+                false,
+                "Post with id {$postId} not found",
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        return new ApiResponse(
+            true,
+            "Post published successfully",
+            JsonResponse::HTTP_OK
+        );
+    }
+
+
+
+
+
+    /**
+     * @Route("/admin/post/unpublish", methods={"PATCH"})
+     */
+    public function unpublish(
+        Request $request,
+        PostRepository $postRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $postId = $request->get("post_id");
+
+        if ($postId === null) {
+            throw new BadRequestHttpException("'post_id' must be present in request body");
+        }
+
+        try {
+            $post = $postRepository->find($postId);
+
+            if ($post === null) {
+                throw new PostNotFoundException();
+            }
+
+            $this->denyAccessUnlessGranted("UNPUBLISH_POST", $post);
+
+            $post->setIsPublished(false);
+
+            $entityManager->flush();
+        } catch (PostNotFoundException) {
+            return new ApiResponse(
+                false,
+                "Post with id {$postId} not found",
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        return new ApiResponse(
+            true,
+            "Post unpublished successfully",
+            JsonResponse::HTTP_OK
         );
     }
 }
